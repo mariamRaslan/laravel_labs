d975a734e199fc2cd1bf8664b2cbaee342b04328
mariamRaslan <mariamraslan@gmail.com>
2022-02-01 15:54:36 +0200

    apply eager loading lab4

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
     
 
     public function index(){
-        $posts=Post::all();
+        $posts=Post::with('user')->get();
         return PostResource::collection($posts);
     }
 
